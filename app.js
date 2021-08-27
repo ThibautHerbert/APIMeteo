5191cc3bec3d1e412d58b0d33382de694646dca6
Thibaut <herbert.thibaut@protonmail.com>
2021-08-27 13:32:07 +0200

    prep structure + test header + footer

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-
+            description: "Besoin de connaître la météo des prochains jours ?",
+            soustexte:"Vous êtes au bon endroit !",
+            services:"Profitez également de nos autres services : réservation de restaurants, hébergements et bien d'autres encore... :"
         }
     }
 })
