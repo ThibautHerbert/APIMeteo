104e1b31f0d51f7d973fbf28ffa611c0496ad8b4
Thibaut <herbert.thibaut@protonmail.com>
2021-08-29 17:54:19 +0200

    test fetch & xmlhttprequest

--- app.js
+++ app.js
@@ -3,9 +3,89 @@ const app = Vue.createApp({
         return {
             description: "Besoin de connaître la météo des prochains jours ?",
             soustexte:"Vous êtes au bon endroit !",
-            services:"Profitez également de nos autres services : réservation de restaurants, hébergements et bien d'autres encore... :"
+            services:"Profitez également de nos autres services : réservation de restaurants, hébergements et bien d'autres encore... :",
+            urlApi: "",
+            meteos: [],
+            nameCity: "",
+            current_condition: "",
+            temperature:"",
+            icon: "",
+            testResultat: "",
+            villeCherchee: ""
         }
-    }
+    },
+    
+    mounted(){
+        fetch('https://www.prevision-meteo.ch/services/json/nantes') // fetch('https://www.prevision-meteo.ch/services/json/' + villeCherchee)
+        .then(res => res.json())
+        //.then(response => this.meteos = response.data)
+        //.then(data => this.meteos = data.city_info) // si utilisée empâche le condition de fonctionner !
+        //.then(data => this.nameCity = data)
+        
+        //.then(data => this.nameCity = data.city_info.name)
+        .then(condition => this.current_condition = condition.current_condition)
+        // marchent pas :
+        .then(temperature => this.temperature = temperature.current_condition.tmp)
+        .then(city => this.nameCity = city)
+        
+        /*
+        let response = JSON.parse(this.responseText);
+        let result = document.querySelector('#weather-result');
+            result.innerHTML = response.current_condition.condition;
+        let nameCity = document.querySelector('#nameCity');
+            nameCity.innerHTML = response.city_info.name;
+        let imgMeteo = document.querySelector('#imgMeteo');
+        let content = `<img src="${response.current_condition.icon}" class="card-img-top" alt="icône météo">`
+            imgMeteo.insertAdjacentHTML("beforeend", content);
+        this.meteos = response;
+*/
+
+       .catch(err => console.log(err.message))
+    },
+    
+    
+    methods: {
+        clicki(){
+            console.log("cliqué");
+            let result = document.querySelector('#weather-result');
+            let btn = document.querySelector('#ask-weather');
+                //result.innerHTML = "bonjour, ça marche";
+                // result.innerHTML = this.testResultat.current_condition.condition; condition pas connu
+                //result.innerHTML = this.testResultat;
+                
+        },
+        callApi() {
+            let askWeather = new XMLHttpRequest();
+            
+            askWeather.open("GET", "https://www.prevision-meteo.ch/services/json/nantes");
+            askWeather.send();
+            askWeather.onreadystatechange = function() {
+                if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+                    let response = JSON.parse(this.responseText);
+                    //let réponse2 = "coucou";
+                    //réponse2 = this.testResultat;
+                    //response = this.testResultat;
+                    //console.log("onreadystate", response.current_condition.condition);
+                    //response = this.testResultat;
+                    let result = document.querySelector('#weather-result');
+                        result.innerHTML = response.current_condition.condition;
+                    let nameCity = document.querySelector('#nameCity');
+                    nameCity.innerHTML = response.city_info.name;
+                    let imgMeteo = document.querySelector('#imgMeteo');
+                    let content = `<img src="${response.current_condition.icon}" class="card-img-top" alt="icône météo">`
+                    imgMeteo.insertAdjacentHTML("beforeend", content);
+                    this.meteos = response;
+                    
+                }
+                
+            }
+            //.then(() => console.log("onreadystate", response.current_condition.condition))
+        },
+        
+        
+
+    },
+    
 })
 
 app.mount('#app')
