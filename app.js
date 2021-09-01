e093a1125acd3f04392465d6f366b66436caad2e
Thibaut <herbert.thibaut@protonmail.com>
2021-09-01 17:29:38 +0200

    input to choose city ok fetch adjusted + cleaning code

--- app.js
+++ app.js
@@ -4,23 +4,20 @@ const app = Vue.createApp({
             description: "Besoin de connaître la météo des prochains jours ?",
             subtext:"Vous êtes au bon endroit !",
             services:"Profitez également de nos autres services : réservation de restaurants, hébergements et bien d'autres encore... :",
-            urlApi: "",
+            urlApi: "https://www.prevision-meteo.ch/services/json/",
             nameCity: "",
             current_condition: "",
-            temperature:"",
-            SearchCityInput: "",
+            searchCityInput: "Nantes",
             meteoNextDay:"",
             meteoThirdDay:"",
             meteoFourthDay:"",
             showNextDay: false,
             showThirdDay: false,
             showFourthDay: false
-
         }
     },
     
     mounted(){
-
         this.fetchCityInfo()
         //appelle la prévision du jour pour la ville:
         this.fetchToday()
@@ -30,76 +27,34 @@ const app = Vue.createApp({
         this.fetchFourthDay()
     },
     methods: {
-        clicki(){
-            console.log("cliqué");
-            let result = document.querySelector('#weather-result');
-            let btn = document.querySelector('#ask-weather');
-                //result.innerHTML = "bonjour, ça marche";
-                // result.innerHTML = this.testResultat.current_condition.condition; condition pas connu
-                //result.innerHTML = this.testResultat;
-                
-        },
-        callApi() {
-            let askWeather = new XMLHttpRequest();
-            
-            askWeather.open("GET", "https://www.prevision-meteo.ch/services/json/nantes");
-            askWeather.send();
-            askWeather.onreadystatechange = function() {
-                if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-                    let response = JSON.parse(this.responseText);
-                    //let réponse2 = "coucou";
-                    //réponse2 = this.testResultat;
-                    //response = this.testResultat;
-                    //console.log("onreadystate", response.current_condition.condition);
-                    //response = this.testResultat;
-                    let result = document.querySelector('#weather-result');
-                        result.innerHTML = response.current_condition.condition;
-                    let nameCity = document.querySelector('#nameCity');
-                    nameCity.innerHTML = response.city_info.name;
-                    let imgMeteo = document.querySelector('#imgMeteo');
-                    let content = `<img src="${response.current_condition.icon}" class="card-img-top" alt="icône météo">`
-                    imgMeteo.insertAdjacentHTML("beforeend", content);
-                    this.meteos = response;   
-                }
-            }
-            //.then(() => console.log("onreadystate", response.current_condition.condition))
-        },
         fetchCityInfo(){
-            fetch('https://www.prevision-meteo.ch/services/json/nantes/city_info') // fetch('https://www.prevision-meteo.ch/services/json/' + villeCherchee)
+            fetch(this.urlApi + this.searchCityInput + '/city_info') 
                 .then(res => res.json())
                 .then(city => this.nameCity = city.city_info)
                 .catch(err => console.log(err.message))
         },
         fetchToday(){
-            fetch('https://www.prevision-meteo.ch/services/json/nantes') // fetch('https://www.prevision-meteo.ch/services/json/' + villeCherchee)
+            fetch(this.urlApi + this.searchCityInput) 
                 .then(res => res.json())
-                //.then(response => this.meteos = response.data)
-                //.then(data => this.meteos = data.city_info) // si utilisée empâche le condition de fonctionner !
-                //.then(data => this.nameCity = data)
-                
-                //.then(data => this.nameCity = data.city_info.name)
                 .then(condition => this.current_condition = condition.current_condition)
-                // marchent pas :
-                .then(temperature => this.temperature = temperature.current_condition.tmp)
-                .then(city => this.nameCity = city)
                 .catch(err => console.log(err.message))
         },
         fetchNextDay() {
-            fetch('https://www.prevision-meteo.ch/services/json/nantes/fcst_day_1') // fetch('https://www.prevision-meteo.ch/services/json/' + villeCherchee)
+            fetch(this.urlApi + this.searchCityInput + '/fcst_day_1') 
                 .then(res => res.json())
                 .then(condition2 => this.meteoNextDay = condition2.fcst_day_1)
                 .then(response => console.log(response))
                 .catch(err => console.log(err.message))
         },
         fetchThirdDay() {
-            fetch('https://www.prevision-meteo.ch/services/json/nantes/fcst_day_2') // fetch('https://www.prevision-meteo.ch/services/json/' + villeCherchee)
+            fetch(this.urlApi + this.searchCityInput + '/fcst_day_2') 
                 .then(res => res.json())
                 .then(condition3 => this.meteoThirdDay = condition3.fcst_day_2)
                 .then(response => console.log(response))
                 .catch(err => console.log(err.message))
         },
         fetchFourthDay() {
-            fetch('https://www.prevision-meteo.ch/services/json/nantes/fcst_day_3') // fetch('https://www.prevision-meteo.ch/services/json/' + villeCherchee)
+            fetch(this.urlApi + this.searchCityInput + '/fcst_day_3') 
                 .then(res => res.json())
                 .then(condition4 => this.meteoFourthDay = condition4.fcst_day_3)
                 .then(response => console.log(response))
